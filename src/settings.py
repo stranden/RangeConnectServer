619082c7a5cbbb05a32b47aa6a0adc76b8a20fa6
Henrik Strand <strand@t-k-l.dk>
2022-06-25 00:30:26 +0200

    Added alembic for SQL migration

--- src/settings.py
+++ src/settings.py
@@ -5,3 +5,10 @@ RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", default="localhost")
 RABBITMQ_USER = os.getenv("RABBITMQ_USER", default="rangeconnect")
 RABBITMQ_PASS = os.getenv("RABBITMQ_PASS", default="Uivq9ACXS49W")
 RABBITMQ_URI = f"amqp://{RABBITMQ_USER}:{RABBITMQ_PASS}@{RABBITMQ_HOST}/"
+
+# PostgreSQL
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+DB_HOST = os.environ.get("DB_HOST")
+DB_NAME = os.environ.get("DB_NAME")
+DB_URI = f"postgres://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
