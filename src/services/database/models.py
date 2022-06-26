fb38fa1aeac48750295880e304365216e1ed9240
Henrik Strand <strand@t-k-l.dk>
2022-06-26 22:09:45 +0200

    Added more DB models

--- src/services/database/models.py
+++ src/services/database/models.py
@@ -17,15 +17,13 @@ from sqlalchemy.orm import relationship
 
 from .base import AuditMixin, Base
 
-class ShootingState(PyEnum):
+class FiringType(PyEnum):
     SIGHTERS = "sighters"
     MATCH = "match"
-    FINISHED = "finished"
-    CANCELLED = "cancelled"
-
-class ShootingType(PyEnum):
     SINGLE_SHOT = "single_shot"
     RAPID_FIRE = "rapid_fire"
+    FINISHED = "finished"
+    CANCELLED = "cancelled"
 
 club_country_association = Table("club_country_association", Base.metadata,
     Column("club_id", ForeignKey("shooting_club.id")),
@@ -65,25 +63,58 @@ class ShootingRange(AuditMixin, Base):
     __tablename__ = "shooting_range"
 
     id = Column(postgresql.UUID(as_uuid=True), primary_key=True, unique=True, index=True, default=uuid.uuid4)
+    club = relationship("ShootingClub", secondary=club_range_association, lazy="joined")
     name = Column(String)
     lanes = Column(Integer)
     first_lane = Column(Integer)
-    club = relationship("ShootingClub", secondary=club_range_association, lazy="joined")
 
 class RangeEventShooter(AuditMixin, Base):
     __tablename__ = "range_event_shooter"
 
     id = Column(postgresql.UUID(as_uuid=True), primary_key=True, unique=True, index=True, default=uuid.uuid4)
-    name = Column(String)
-    lane_id = Column(Integer)
-    range_shooter_id = Column(Integer)
     shooting_range = relationship("ShootingRange", secondary=range_shooter_association, lazy="joined")
+    range_shooter_id = Column(Integer)
+    name = Column(String)
+    team = Column(String)
+    class_ = Column('class', String)
+    lane_id = Column(Integer)
 
-class RangeEventTeam(AuditMixin, Base):
-    __tablename__ = "range_event_team"
+class RangeEventPractice(AuditMixin, Base):
+    __tablename__ = "range_event_practice"
 
     id = Column(postgresql.UUID(as_uuid=True), primary_key=True, unique=True, index=True, default=uuid.uuid4)
-    name = Column(String)
-    lane_id = Column(Integer)
-    range_shooter_id = Column(Integer)
     shooting_range = relationship("ShootingRange", secondary=range_shooter_association, lazy="joined")
+    range_shooter_id = Column(Integer)
+    sequence_number = Column(Integer)
+    timestamp = Column(TIMESTAMP, nullable=False, server_default=func.now())
+    event_type = Column(Integer)
+    practice_sequence_number = Column(Integer)
+    shoot_code = Column(Integer)
+    practice_code = Column(Integer)
+    
+class RangeEventGroup(AuditMixin, Base):
+    __tablename__ = "range_event_group"
+
+    id = Column(postgresql.UUID(as_uuid=True), primary_key=True, unique=True, index=True, default=uuid.uuid4)
+    shooting_range = relationship("ShootingRange", secondary=range_shooter_association, lazy="joined")
+    range_shooter_id = Column(Integer)
+    sequence_number = Column(Integer)
+    timestamp = Column(TIMESTAMP, nullable=False, server_default=func.now())
+    event_type = Column(Integer)
+    group_ordinal = Column(Integer)
+    firing_type = Column(Enum(FiringType), nullable=False, index=True)
+    expected_number_of_shots = Column(Integer)
+    
+class RangeEventGroup(AuditMixin, Base):
+    __tablename__ = "range_event_group"
+
+    id = Column(postgresql.UUID(as_uuid=True), primary_key=True, unique=True, index=True, default=uuid.uuid4)
+    shooting_range = relationship("ShootingRange", secondary=range_shooter_association, lazy="joined")
+    range_shooter_id = Column(Integer)
+    sequence_number = Column(Integer)
+    timestamp = Column(TIMESTAMP, nullable=False, server_default=func.now())
+    event_type = Column(Integer)
+    group_ordinal = Column(Integer)
+    firing_type = Column(Enum(FiringType), nullable=False, index=True)
+    expected_number_of_shots = Column(Integer)
+    
