83ff5fefb16e66d531abda6170708e7ca6fdc665
Henrik Strand <strand@t-k-l.dk>
2023-01-31 22:24:12 +0100

    Added code for doing a POC subscriber - needs testing

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
 import asyncio
-from asyncore import loop
 import settings
+import services.messaging.subscriber
 from util import *
+
+async def rabbitmq_subscriber():
+    
+    Subscriber = services.messaging.subscriber.Subscriber(settings.RABBITMQ_URI)
+    await Subscriber.subscribe_range_events()
 
 async def main():
     logging.info("Starting RangeConnectServer")
+
+    logging.info("Creating RabbitMQ subscriber task")
+    task_rabbitmq_subscriber = asyncio.create_task(
+       rabbitmq_subscriber()
+    )
+
+    logging.info("Executing RabbitMQ subscriber task")
+    await task_rabbitmq_subscriber
 
 asyncio.run(main())
--- src/services/messaging/subscriber.py
+++ src/services/messaging/subscriber.py
@@ -1,20 +1,32 @@
 import json
+import asyncio
 import aio_pika
+from aio_pika.abc import AbstractIncomingMessage
+
 
 class Subscriber:
 
     def __init__(self, amqp_connection_uri: str) -> None:
         self.amqp_connection_uri = amqp_connection_uri
 
-    async def subscribe_range_events(self, message: dict) -> None:
+    async def proccess_message(message: AbstractIncomingMessage) -> None:
+        async with message.process():
+            print(message.body)
+            await asyncio.sleep(1)
+
+    async def subscribe_range_events(self) -> None:
         connection = await aio_pika.connect(self.amqp_connection_uri)
         queue_name = "shooting_range_events"
-        routing_key = queue_name
         async with connection:
             # Creating a channel
             channel = await connection.channel()
+            await channel.set_qos(prefetch_count=1)
 
             # Declaring queue
-            await channel.declare_queue(
-                queue_name
+            queue = await channel.declare_queue(
+                queue_name,
+                durable=True
             )
+
+            # Consume messages
+            await queue.consume(proccess_message)
