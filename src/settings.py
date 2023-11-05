f17e694f0f9aa433dd8b24da105438bb6ffeea0b
stranden <henrik@hens.dk>
2023-11-05 22:18:49 +0100

    Prerequisites for database support

--- src/settings.py
+++ src/settings.py
@@ -6,3 +6,9 @@ RABBITMQ_USER = os.getenv("RABBITMQ_USER", default="rangeconnect")
 RABBITMQ_PASS = os.getenv("RABBITMQ_PASS", default="Uivq9ACXS49W")
 RABBITMQ_URI = f"amqp://{RABBITMQ_USER}:{RABBITMQ_PASS}@{RABBITMQ_HOST}/"
 
+# PostgreSQL
+DATABASE_HOST = os.getenv("DATABASE_HOST", default="localhost")
+DATABASE_USER = os.getenv("DATABASE_USER", default="rangeconnect")
+DATABASE_PASS = os.getenv("DATABASE_PASS", default="MzqGbe#o8AUn")
+DATABASE_DB = os.getenv("DATABASE_DB", default="rangeconnect")
+DATABASE_URI = f"postgresql://{DATABASE_USER}:{DATABASE_PASS}@{DATABASE_HOST}/{DATABASE_DB}"
