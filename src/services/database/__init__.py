dedad5d9630a38f838ad6dfbf2940f6661af017a
stranden <henrik@hens.dk>
2023-11-05 22:45:01 +0100

    Initial commit to __init__.py

---
+++ src/services/database/__init__.py
@@ -0,0 +1,9 @@
+import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+from .models import *
+
+engine = create_engine(settings.DATABASE_URI, pool_pre_ping=True, future=True, echo=True) # echo=True to view sql queries in logs
+
+DBSession: Session = sessionmaker(bind=engine, future=True)
