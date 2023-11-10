6fd4b7e4564a2bc7aa3e1a1b3739fe5f8bae8735
stranden <henrik@hens.dk>
2023-11-10 20:52:50 +0100

    Added some SQL queries

--- src/services/database/models.py
+++ src/services/database/models.py
@@ -24,7 +24,7 @@ class Country(Base):
     name = Column(String, nullable=False)
 
 
-class Country(Base):
+class ShootingClub(Base):
     __tablename__ = "shooting_club"
 
     id = Column(postgresql.UUID(as_uuid=True), primary_key=True, unique=True, index=True, default=uuid.uuid4)
@@ -32,3 +32,28 @@ class Country(Base):
     name = Column(String, nullable=False)
     shortname = Column(String, nullable=True)
 
+
+class Event(Base):
+    __tablename__ = "event"
+
+    id = Column(postgresql.UUID(as_uuid=True), primary_key=True, unique=True, index=True, default=uuid.uuid4)
+    shooting_club = Column(postgresql.UUID(as_uuid=True), ForeignKey("shooting_club.id"), nullable=True, index=True)
+    name = Column(String, nullable=False)
+    startdate =  Column(TIMESTAMP, nullable=False)
+    enddate =  Column(TIMESTAMP, nullable=False)
+    staus = Column(Integer, nullable=False, default=0)
+
+
+class EventRange(Base):
+    __tablename__ = "event_range"
+
+    id = Column(postgresql.UUID(as_uuid=True), primary_key=True, unique=True, index=True, default=uuid.uuid4)
+    event = Column(postgresql.UUID(as_uuid=True), ForeignKey("event.id"), nullable=True, index=True)
+    shooting_range = Column(postgresql.UUID(as_uuid=True), ForeignKey("shooting_range.id"), nullable=True, index=True)
+
+
+class RangeManufactor(AuditMixin, Base):
+    __tablename__ = "manufactor"
+
+    id = Column(postgresql.UUID(as_uuid=True), primary_key=True, unique=True, index=True, default=uuid.uuid4)
+    name = Column(String, nullable=False)
