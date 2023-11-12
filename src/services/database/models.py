ae0fb651cea2b93f1efc3fbf4cbd0823fb879411
stranden <henrik@hens.dk>
2023-11-12 21:28:01 +0100

    Added RangeEventShot model

--- src/services/database/models.py
+++ src/services/database/models.py
@@ -2,19 +2,26 @@ import uuid
 from enum import Enum as PyEnum
 
 from sqlalchemy import (
-    TIMESTAMP,
+    
     Column,
     Enum,
+    Float,
     ForeignKey,
+    func,
     Integer,
     String,
     Table,
-    func
+    TIMESTAMP
 )
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import relationship
 
 from .base import AuditMixin, Base
+
+class SeriesType(PyEnum):
+    SIGHT = "sight"
+    MATCH = "match"
+
 
 class Country(Base):
     __tablename__ = "country"
@@ -91,4 +98,18 @@ class RangeEventShooter(AuditMixin, Base):
     club = Column(String, nullable=True)
     group = Column(String, nullable=True)
     first_lane = Column(String, nullable=False)
+
+
+class RangeEventShot(AuditMixin, Base):
+    __tablename__ = "range_event_shot"
+
+    id = Column(postgresql.UUID(as_uuid=True), primary_key=True, unique=True, index=True, default=uuid.uuid4)
+    shooting_range = Column(postgresql.UUID(as_uuid=True), ForeignKey("shooting_range.id"), nullable=False, index=True)
+    start_number = Column(Integer, nullable=False)
+    series_type = Column(Enum(SeriesType), nullable=False)
+    shot_id = Column(Integer, nullable=False)
+    shot_value = Column(Integer, nullable=False)
+    shot_value_decimal = Column(Float, nullable=False)
+    x_coord = Column(Float, nullable=False)
+    y_coord = Column(Float, nullable=False)
 
