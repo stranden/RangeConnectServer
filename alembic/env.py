ca8b7d9f0eba879f0a56e39f33c937af89f50813
stranden <henrik@hens.dk>
2023-12-17 15:25:01 +0100

    Removed models used for another project

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from src.services.database.base import Base
+from src.services.database.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
