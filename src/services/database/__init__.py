15239e77ef0fbfb0bb5a77bd4885cb8ab7aa54ed
stranden <henrik@hens.dk>
2023-12-18 14:47:50 +0100

    Commited work

--- src/services/database/__init__.py
+++ src/services/database/__init__.py
@@ -2,7 +2,8 @@ import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
-from .models import *
+from .models import * # noqa
+
 
 engine = create_engine(settings.DATABASE_URI, pool_pre_ping=True, future=True) # echo=True to view sql queries in logs
 
